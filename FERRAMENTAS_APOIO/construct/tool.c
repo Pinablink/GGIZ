4026999674fbf169f3d7a05e33530ff58a5dd584
Pinablink <pinablink@hotmail.com>
2020-04-10 00:56:39 -0300

    Inclusao de diretorio contendo ferramenta de apoio

---
+++ FERRAMENTAS_APOIO/construct/tool.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../header/header.h"
+
+unsigned int main (void) {
+     unsigned int linhas = 0;
+     unsigned int colunas = 0;
+
+     printf("%s", INIT_WELCOME); 
+     system("PAUSE");
+     printf("%s", QT_LINHAS);
+     printf("%s", QT_COLUNAS);   
+
+
+    return 0;
+}
---
+++ FERRAMENTAS_APOIO/header/header.h
@@ -0,0 +1,3 @@
+#define INIT_WELCOME "Este aplicativo e uma ferramenta para gerar uma tabela\ncom div HTML seguindo o padrao utilizado no GGIZ.\n\n"
+#define QT_LINHAS "\nnInforme a quantidade de linhas."
+#define QT_COLUNAS "\nInforme a quantidade de colunas."
