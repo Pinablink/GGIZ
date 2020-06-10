940307743832f02ff3ae4377405192aefaee965a
pinablink <pinablink@hotmail.com>
2020-06-10 17:41:07 -0300

    Inclusao de primeira versao

---
+++ GGIZ_WEB/ggizWeb/src/main/java/br/com/ggiz/ggizWeb/GgizWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.ggiz.ggizWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgizWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgizWebApplication.class, args);
+	}
+
+}
