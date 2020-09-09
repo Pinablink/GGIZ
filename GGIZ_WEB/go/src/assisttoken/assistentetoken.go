9806d4fe3948b09f6301865da4244fef10230905
pinablink <pinablink@hotmail.com>
2020-09-08 22:07:45 -0300

    Atualização do agente de segurança

--- GGIZ_WEB/go/src/assisttoken/assistentetoken.go
+++ GGIZ_WEB/go/src/assisttoken/assistentetoken.go
@@ -11,7 +11,14 @@ import (
 func main() {
 	var strPortServe string = os.Getenv("KEY_SEED_PORT_TOKEN_GGIZ")
 	var ggizRedisPath string = os.Getenv("GGIZ_REDIS_PATH_ENVIRONMENT")
-	netinput.InitCtrServerToken(ggizRedisPath)
-	fmt.Println("GGIZ - Assistente de Seguranca")
-	log.Fatal(http.ListenAndServe(strPortServe, nil))
+
+	if len(strPortServe) > 0 && len(ggizRedisPath) > 0 {
+		fmt.Println("Recursos do Ambiente obtidos com sucesso")
+		netinput.InitCtrServerToken(ggizRedisPath)
+		fmt.Println("GGIZ - Assistente de Seguranca")
+		log.Fatal(http.ListenAndServe(strPortServe, nil))
+	} else {
+		fmt.Println("Ocorreu um problema na obtenção das variaveis de ambiente...")
+	}
+
 }
